03021cf04569c4840e31530cd7ee197fc6249cc7
sunshj <sunshj317@qq.com>
2023-12-21 10:12:44 +0800

    ci: 添加husky, lint-staged

--- scripts/prebuild.cjs
+++ scripts/prebuild.cjs
@@ -1,23 +1,19 @@
-const fs = require('node:fs/promises')
+const fs = require('node:fs')
 const path = require('node:path')
 
-async function prebuild() {
-  const corePkgPath = path.join(__dirname, '../packages/core/package.json')
-  const pkg = (await fs.readFile(corePkgPath)).toString()
-  const pkgJson = JSON.parse(pkg)
+const corePkgPath = path.join(__dirname, '../packages/core/package.json')
+const pkg = fs.readFileSync(corePkgPath).toString()
+const pkgJson = JSON.parse(pkg)
 
-  pkgJson.main = 'dist/index.cjs'
-  pkgJson.module = 'dist/index.mjs'
-  pkgJson.types = 'dist/index.d.ts'
-  pkgJson.exports = {
-    '.': {
-      types: './dist/index.d.ts',
-      require: './dist/index.cjs',
-      import: './dist/index.mjs'
-    }
+pkgJson.main = 'dist/index.cjs'
+pkgJson.module = 'dist/index.mjs'
+pkgJson.types = 'dist/index.d.ts'
+pkgJson.exports = {
+  '.': {
+    types: './dist/index.d.ts',
+    require: './dist/index.cjs',
+    import: './dist/index.mjs'
   }
-
-  await fs.writeFile(corePkgPath, JSON.stringify(pkgJson, null, 2))
 }
 
-prebuild()
+fs.writeFileSync(corePkgPath, JSON.stringify(pkgJson, null, 2))
